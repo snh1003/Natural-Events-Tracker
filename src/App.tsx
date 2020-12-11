2d609592c23a9a4fdfc85adea599afbf2c0404fe
snh1003 <lunasaprismriver57@gmail.com>
2020-12-11 23:48:42 +0900

    mapbox test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-function App() {
+mapboxgl.accessToken = 'pk.eyJ1Ijoic25oMTAwMyIsImEiOiJja2lrZDdra3EwMWU5MzNvNjY5eDV1a210In0.TSJVONM0eRcO4JCm4Ff7aQ'
+const App = () => {
+  const mapContainerRef = useRef(null);
+
+  const [lng, setLng] = useState(5);
+  const [lat, setLat] = useState(34);
+  const [zoom, setZoom] = useState(1.5);
+
+  // Initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+
+    return () => map.remove();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div className='sidebarStyle'>
+          <div>
+            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+          </div>
+        </div>
+        <div className='map-container' ref={mapContainerRef} />
+      </div>
   );
-}
+};
 
 export default App;
